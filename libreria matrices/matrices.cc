445ce0f50d7dcb07887ad72067d77287f47ae4f0
FHP <a52hepof@uco.es>
2018-11-12 08:20:02 +0100

    cambios carpetas

---
+++ libreria matrices/matrices.cc
@@ -0,0 +1,30 @@
+//matrices.cc
+
+#include "matrices.h"
+#include <iostream>
+
+
+using namespace matrices;
+using namespace std;
+
+void Matrixes::matrixMemory(Matrixes &matrix,int nrows, int ncolumns){
+
+	/*
+	matrix = new int *[nrows];
+    for(int i = 0; i<ncolumns;i++){
+      	matrix[i] = new int[ncolumns];
+    }
+	*/
+}
+
+Matrixes Matrixes::matrixMemory(int nrows, int ncolumns){
+
+
+}
+
+int main(){
+
+
+	return 0;
+
+}
